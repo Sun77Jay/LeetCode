14a3973620fdeaa5c919d9edb518d3b17a5dfe23
Sun77Jay <godzilllla77@gmail.com>
2023-10-31 20:56:18 -0400

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+/**merge strings alternately
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let res = ''
+    let maxLength = Math.max(word1.length, word2.length)
+ 
+    for (let i=0; i < maxLength; i++) {
+        if (word1[i]) res += word1[i]
+        if (word2[i]) res += word2[i]
+    }
+ 
+    return res
+ };
